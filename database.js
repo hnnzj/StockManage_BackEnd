ca203a9bee6eb616cea20738c63d7b33ecc12653
hnnzj <heinnitzj@gmail.com>
2023-11-01 02:27:29 -0300

    Ready to deploy - 3

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: `${process.env.HOST}`,
-//     dialect: "postgres",
-//   }
-// );
 const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: `${process.env.HOST}`,
+    dialect: "mysql",
+  }
 );
+// const sequelize = new Sequelize(
+//   `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// );
 const Users = require("./database/models/users")(sequelize);
 const Client = require("./database/models/clientes")(sequelize);
 const Product = require("./database/models/stock")(sequelize);
