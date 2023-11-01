df46fae70e4683108f59ff95316f213cad6914e7
hnnzj <heinnitzj@gmail.com>
2023-11-01 01:10:25 -0300

    Ready to deploy - 2

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var bodyParser = require("body-parser");
 
 const cors = require("cors");
 const app = express();
-const port = 5001;
+const port = process.env.P0RT || 5001;
 
-const user_Routes = require("/api/routes/userRoutes");
-const product_Routes = require("/api/routes/productRoutes");
-const clientes_Routes = require("/api/routes/clientesRoutes");
-const pedidos_Routes = require("/api/routes/pedidosRoutes");
+const user_Routes = require("../api/routes/userRoutes");
+const product_Routes = require("../api/routes/productRoutes");
+const clientes_Routes = require("../api/routes/clientesRoutes");
+const pedidos_Routes = require("../api/routes/pedidosRoutes");
 
 app.use(bodyParser.json());
 app.use(cors());
