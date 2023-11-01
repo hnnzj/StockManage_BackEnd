1d32a2ec08fd50acbc5fd426fa5c1c7719374d55
hnnzj <heinnitzj@gmail.com>
2023-11-01 02:06:41 -0300

    Ready to deploy - 3

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: `${process.env.DB_HOST}:${process.env.DB_PORT}`,
+    host: `${process.env.HOST}:${process.env.DB_PORT}`,
     dialect: "postgres",
   }
 );
